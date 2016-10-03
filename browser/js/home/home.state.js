51208fd5d581ccac21ce0140c3e904609e1c8d02
Pierre Baize <pierrebaize@Pierres-MacBook-Pro.local>
2016-10-03 15:58:14 -0400

    getting location and bunker id on prompt

--- browser/js/home/home.state.js
+++ browser/js/home/home.state.js
@@ -2,6 +2,19 @@ app.config(function ($stateProvider) {
   $stateProvider.state('master.navbar.home', {
     url: '/',
     templateUrl: 'js/home/home.html',
-    controller: 'HomeController'
+    controller: 'HomeController',
+    resolve: {
+      promptBunkerCreate: function (BunkerStateFactory, AuthService) {
+        return AuthService.getLoggedInUser()
+          .then(user => {
+            if (!user) return false
+            return BunkerStateFactory.getBunkerState(user.id)
+          })
+          .then(bunker => {
+            console.log(bunker)
+            return bunker.noBunker || false
+          })
+      }
+    }
   })
 })

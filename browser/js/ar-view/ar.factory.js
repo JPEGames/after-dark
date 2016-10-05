a7973acf01f01ecc8fea9b518dd1bdeb161c4383
Pierre Baize <pierrebaize@Pierres-MacBook-Pro.local>
2016-10-05 16:17:45 -0400

    refactored AR factory

--- browser/js/ar-view/ar.factory.js
+++ browser/js/ar-view/ar.factory.js
@@ -9,10 +9,6 @@ app.factory('ArFactory', function (DistanceFactory) {
 
   ArFactory.getMenuView = () => showMenu
 
-  ArFactory.diff = (loc, prev, sensitivity = 0) => prev
-    ? DistanceFactory.greaterThanXM(loc, prev, sensitivity)
-    : true
-
   ArFactory.getCurrentPosition = () => {
     return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -24,16 +20,6 @@ app.factory('ArFactory', function (DistanceFactory) {
       })
     })
   }
-  ArFactory.makeLocationWatcher = function (func, sensitivity = 0) {
-    function success (geoObj) {
-      let loc = {lat: geoObj.coords.latitude, lng: geoObj.coords.longitude}
-      if (ArFactory.diff(loc, prevLoc, sensitivity)) {
-        func(loc)
-        ArFactory.coords = loc.coords
-        prevLoc = loc
-      }
-    }
-    navigator.geolocation.watchPosition(success, console.warn, {enableHighAccuracy: true})
-  }
+
   return ArFactory
 })

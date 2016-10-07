4d5e956f0a81b47c4c699209039669e5d19e4644
jyyeh77 <jyyeh77@gmail.com>
2016-10-07 15:56:06 -0400

    event table and functions done

--- server/db/models/point/loot.js
+++ server/db/models/point/loot.js
@@ -1,1 +1,12 @@
-// module.exports
+// table used to generate events for points
+
+let lootTable = {
+  '0.35': 'scrap metal',
+  '0.4': 'rare relic',
+  '0.7': 'rat attack',
+  '0.75': 'dust storm',
+  '0.9': 'mutant attack',
+  '1': 'information'
+}
+
+module.exports = lootTable

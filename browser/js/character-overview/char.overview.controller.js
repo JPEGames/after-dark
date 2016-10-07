722ced5c824cfad7fdcf93410cdef4e45e830f82
Eliot S <eliotpszw@gmail.com>
2016-10-07 14:55:50 -0400

    fixed progress bar and added title and subtitle

--- browser/js/character-overview/char.overview.controller.js
+++ browser/js/character-overview/char.overview.controller.js
@@ -2,6 +2,11 @@ app.controller('CharOverviewController', function ($scope, $state, CharacterFact
   $scope.myChar = myCharacter
   console.log($scope.myChar)
   // myChar.perk lol
-  $scope.myExpPer = 100
+  $scope.myExpPer = '10%'
   $scope.myProgress = {'width': $scope.myExpPer}
+  let nameSplit = $scope.myChar.perk.split('')
+  nameSplit[0] = nameSplit[0].toUpperCase()
+  nameSplit = nameSplit.join('')
+  $scope.myTitle = nameSplit
+  $scope.myLocation = 'New York'
 })

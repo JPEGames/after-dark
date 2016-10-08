5053cc4463d408f7ec4887b355a75fec76d3efb7
jyyeh77 <jyyeh77@gmail.com>
2016-10-07 20:27:41 -0400

    bunker post

--- server/app/routes/bunkerstate/index.js
+++ server/app/routes/bunkerstate/index.js
@@ -3,6 +3,9 @@ const router = require('express').Router()
 const db = require('../../../db')
 const Bunker = db.model('bunker')
 const User = db.model('user')
+const GeoFire = require('geofire')
+const firebaseRef = require('../../../db/firebase')
+let geofireRef = new GeoFire(firebaseRef.child('locations'))
 module.exports = router
 
 // for getting bunker associated with logged-in user
@@ -28,14 +31,25 @@ router.get('/:id', function (req, res, next) {
 })
 
 // get or create a bunker associated with signed-in user
-router.get('/:id/newBunker', function (req, res, next) {
-  Bunker.findOrCreate({
-    where: {
-      userId: req.requestedUser.id
-    }
-  })
-    .then(userBunker => {
-      !userBunker[0] ? res.sendStatus(500) : res.send(userBunker[0])
+// router.get('/:id/newBunker', function (req, res, next) {
+//   Bunker.findOne({
+//     where: {
+//       userId: req.requestedUser.id
+//     }
+//   })
+//     .then(userBunker => {
+//       !userBunker[0] ? res.sendStatus(500) : res.send(userBunker[0])
+//     })
+//     .catch(next)
+// })
+
+// makes new bunker
+router.post('/:id', function (req, res, next, id) {
+  Bunker.create(req.body)
+    .then(newBunker => {
+      if (!newBunker) res.sendStatus(404)
+      geofireRef.set(`bunker_${newBunker.id}`, newBunker)
+      res.send(newBunker)
     })
     .catch(next)
 })

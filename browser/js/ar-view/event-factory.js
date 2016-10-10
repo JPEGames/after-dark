c8e463d0840b8d766ab1d508f96ea3325903f026
jyyeh77 <jyyeh77@gmail.com>
2016-10-10 12:16:10 -0400

    working on backpack

--- browser/js/ar-view/event-factory.js
+++ browser/js/ar-view/event-factory.js
@@ -1,12 +1,16 @@
 app.factory('EventFactory', function ($http, AuthService, FbFactory, GeoFireFactory) {
   let EventFactory = {}
-  const resources = ['scrap metal', 'H2O']
+  const resources = [ 'scrap metal', 'H2O' ]
 
   // adds resource to database
   EventFactory.resourceToBackpack = (eventData) => {
     if (resources.includes(eventData.type)) {
       console.log('in backpack: ', eventData.type)
-      // return $http.put()
+      var newResource = eventData.type
+      return AuthService.getLoggedInUser()
+        .then(user => {
+          return $http.put(`/api/backpack/${user.id}`, {newResource})
+        })
     }
   }
   return EventFactory

a4db8688a652ebf4a6c83fc837b122f0aa07b678
Eliot S <eliotpszw@gmail.com>
2016-10-13 17:27:40 -0400

    BEAUTIFUL GORGEOUS ABSOLUTELY DELIGHTFUL VANILLA JS EXP EFFECTS INTEGRATED. PROJECT COMPLETE.

--- browser/js/common/directives/navbar/NavbarFactory.js
+++ browser/js/common/directives/navbar/NavbarFactory.js
@@ -1,4 +1,4 @@
-app.factory('NavbarFactory', function ($http) {
+app.factory('NavbarFactory', function ($http, $interval) {
   let NavbarFactory = {}
   let hasCharacter = null
   let hasBunker = null
@@ -9,6 +9,38 @@ app.factory('NavbarFactory', function ($http) {
   NavbarFactory.setter = (characterState, bunkerState) => {
     hasCharacter = characterState
     hasBunker = bunkerState
+  }
+  NavbarFactory.createExp = function (expAmount) {
+    var posx = window.mouseXY.x
+    var posy = window.mouseXY.y
+    if (!e) var e = window.event
+
+    posx = e.clientX
+    posy = e.clientY
+
+    console.log('X: ' + posx)
+    console.log('Y: ' + posy)
+
+    if (document.getElementById('textFloat')) {
+      document.getElementById('textFloat').remove()
+    }
+
+    var newDiv = document.createElement('div')
+    newDiv.setAttribute('id', 'textFloat')
+    document.body.appendChild(newDiv)
+
+    newDiv.innerHTML = '+' + expAmount + ' Exp'
+    newDiv.style = `z-index: 100011; top: ` + posy + `px; left: ` + (posx - 25) + `px; display: block; position: absolute; transition: top 3s, opacity 3s;`
+
+    console.log(newDiv)
+
+    let moveUp = function () {
+      newDiv.style.top = posy - 100 + 'px'; newDiv.style.opacity = 0
+    }
+
+    $interval(moveUp, 100, 1)
+
+    console.log('Called createExp!')
   }
   return NavbarFactory
 })

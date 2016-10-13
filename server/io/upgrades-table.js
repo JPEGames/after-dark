6813e190f7379775e1ddf7f6d525244728ad5f32
jyyeh77 <jyyeh77@gmail.com>
2016-10-13 17:37:05 -0400

    for pierre

--- server/io/upgrades-table.js
+++ server/io/upgrades-table.js
@@ -1,13 +1,18 @@
 const Bunker = require('../db/models/bunker')
 const Sequelize = require('sequelize')
+const calculations = require('../app/routes/gamestate/calculations')
+const _ = require('lodash')
 module.exports = {}
-function resourceCheck (bunkerResourceObj, id) {
+function resourceCheck (upgradeReqs, id) {
   Bunker.find({
     where: {
       userId: id
     }
   })
     .then(bunker => {
-
+      let resources = calculations(Bunker)
+      // for (let resource in resources) {
+      //
+      // }
         })
 }

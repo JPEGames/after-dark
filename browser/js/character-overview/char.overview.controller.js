ed1820fd79bc4115ffe5ab664c760d6c06dc72e5
Eliot S <eliotpszw@gmail.com>
2016-10-15 15:34:51 -0400

    added dynamic stats for lvls, bit messy need input on how best to organize all that

--- browser/js/character-overview/char.overview.controller.js
+++ browser/js/character-overview/char.overview.controller.js
@@ -1,8 +1,12 @@
 app.controller('CharOverviewController', function ($scope, $state, NavbarFactory, CharacterFactory, myCharacter, $rootScope) {
+  // ELIOT - BIT MESSY - need to decide where logic goes, directive or controller
+  // right now its a mix.
   $scope.myChar = myCharacter
   console.log($scope.myChar)
-  // myChar.perk lol
+  $scope.myLevel = 1
   $scope.myExpPer = '10%'
+  $scope.myMaxExp = 100
+  $scope.myExp = 10
   $scope.myProgress = {'width': $scope.myExpPer}
   let nameSplit = $scope.myChar.perk.split('')
   nameSplit[0] = nameSplit[0].toUpperCase()
